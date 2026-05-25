804cdfa7cee9e7af35cd3fcfb773acc159e717b1
osama1427os-tech <osama1427os@gmail.com>
2026-05-25 20:03:37 +0300

    feat: design contact us page

--- app.js
+++ app.js
@@ -10,7 +10,25 @@ app.use("/static", express.static(path.join(__dirname, "static")));
 app.get("/", (req, res) => {
     res.render("index");
 });
+// صفحة المتجر
+app.get('/shop', (req, res) => {
+    res.render('shop'); 
+});
 
+// صفحة العروض
+app.get('/offers', (req, res) => {
+    res.render('offers'); 
+});
+
+// صفحة تواصل معنا
+app.get('/contact', (req, res) => {
+    res.render('contact'); 
+});
+
+// صفحة السلة
+app.get('/cart', (req, res) => {
+    res.render('cart'); 
+});
 const PORT = 3000;
 
 app.listen(PORT, () => {
